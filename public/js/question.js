3c3a7c9b982f63139e21688abe43c5039e42c6a1
susan <susan@users.noreply.github.com>
2020-09-02 16:43:03 -0400

    backend for comments votes

--- public/js/question.js
+++ public/js/question.js
@@ -2,7 +2,7 @@
 
 const voteContainerTag = document.querySelector(".js-vote-container");
 
-const serverURL='/comments/10/vote/';
+const serverURL='https://localhost:8000/comments/10/vote/';
 
 function selectArrow(event) {
    let direction= ""; 
@@ -16,11 +16,27 @@ function selectArrow(event) {
        plus = -1;
      }
    let voteTotalTag = event.target.parentNode.parentNode.querySelector(".vote-total");
-   let count = Number(voteTotalTag.innerText.substring(1));
-   count = count + plus;
-   let newText = `+ ${count}`;
-   voteTotalTag.innerText = newText;
+  //  let count = Number(voteTotalTag.innerText.substring(1));
+  //  count = count + plus;
+  //  let newText = `+ ${count}`;
+  //  voteTotalTag.innerText = newText;
+
+   fetch(`${serverURL}${direction}`, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: {JSON.stringify()}
+   })
+   .then(r => r.json())
+   .then(r => {
+    let count = r['votes'];
+    let newText = `+ ${count}`;
+    voteTotalTag.innerText = newText;
+    }
+   )
 }
 
-
 voteContainerTag.addEventListener("click", selectArrow);
+
